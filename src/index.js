a03f9324f57fc9e71c59863954639d8b067aae9d
aswin-pro <aswinramesh2016@gmail.com>
2024-08-09 16:58:08 +0530

    project updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root=ReactDOM.createRoot(document.getElementById("root"))
+ function NumberGuessGame(){
+
+  const [enterednum,setenterednum]=useState("")
+  const [output,setoutput]=useState("")
+  const [outputcolor,setoutputcolor]=useState("")
+  const [score,setscore]=useState(0)
+  const [total,settotal]=useState(0)
+
+  const correctans=Math.floor(Math.random()*5)+1
+  function handleEnum(evt){
+      setenterednum(evt.target.value)
+  }
+  function check(){
+
+      if(Number(enterednum)===correctans){
+        setoutput("Awesome! That's the correct number!")
+        setoutputcolor("green")
+        setscore(score+1)
+        settotal(total+1)
+        setenterednum("")
+      }
+      else
+      {
+        setoutput("Incorrect guess. Give it another shot!")
+        setoutputcolor("red")
+        setenterednum("")
+        settotal(total+1)
+      }
+  }
+  return(
+    <div className='main'>
+      <h1>QuickGuess</h1>
+      <p>Guess a Number Between 1 to 5</p>
+      <input type="number" placeholder='Enter a Number 1 to 5' value={enterednum} onChange={handleEnum} /><br />
+      <h4 style={{color:outputcolor}}>{output}</h4>
+      <button onClick={check}>Check</button><br />
+      <h3>Score: {score} Out of {total} </h3>
+    </div>
+  )
+ }
+
+export default NumberGuessGame;
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <NumberGuessGame/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
